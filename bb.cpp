872a047534bd1680fbbb6ba3853fb844c0522b24
Dominick Weston <DominickSWeston@outlook.com>
2025-04-18 13:25:33 -0500

    added states

--- bb.cpp
+++ bb.cpp
@@ -1,10 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
+    vector<int> tape(10, 0);
+    char state = 'A';
+    int head = 5;
+
+    for (int i = 0; i < (int)tape.size(); i++) {
+        cout << tape[i];
+    }
+    cout << endl;
+
+    while (true)
+    {
+        int symbol = 0;
+        if (state == 'A')
+        {
+            if (symbol == 0)
+            {
+                tape[head] = 1;
+                head += 1;
+                state = 'B';
+            }
+        }
+        if (state == 'A')
+        {
+            if (symbol == 1)
+            {
+                tape[head] = 1;
+                head -= 1;
+                state = 'B';
+            }
+        }
+        if (state == 'B')
+        {
+            if (symbol == 0)
+            {
+                tape[head] = 1;
+                head -= 1;
+                state = 'A';
+            }
+        }
+        if (state == 'B')
+        {
+            if (symbol == 1)
+            {
+                tape[head] = 1;
+                head += 1;
+                state = 'H';
+                break;
+            }
+        }
+        for (int i = 0; i < (int)tape.size(); i++) {
+            cout << tape[i];
+        }
+        cout << endl;
+        std::this_thread::sleep_for(2000ms);
+    }
+
     return 0;
 }
